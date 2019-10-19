7c26f7f42f5c14e6961bcf16986f031e5d13db07
Mayur Karmarkar <mayurk2244@gmail.com>
2019-10-19 20:25:29 +0530

    : heavy_check_mark: Initial Commit :heavy_check_mark: Necessary File created for the project

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = new express();
+const fs = require("fs");
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/get/rooms", (req, res) => {
+  let rooms_details = fs.readFileSync(__dirname + "/data/room.json");
+  rooms_details = JSON.parse(rooms_details);
+  res.json(rooms_details);
+});
+
+io.on("connection", function(socket) {
+  socket.on("room_status_update", function(data) {
+    socket.broadcast.emit("room_status_update", data);
+  });
+});
+
+app.get("/manage_rooms", (req, res) => {
+  res.sendFile(__dirname + "/views/manage_room.html");
+});
+
+// app.listen(2000);
+server.listen(80);
