8d13459036debb2a86980dff65484993fc9accb3
Mayur Karmarkar <mayurk2244@gmail.com>
2020-08-11 20:02:38 +0530

    📝 Changes ✔️ UI Design updated ✔️ Removed necessary code

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = new express();
 const fs = require("fs");
+const favicon = require("serve-favicon");
 
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
-app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + "/node_modules"));
+app.use(favicon(__dirname + "/favicon.ico"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
@@ -17,15 +19,19 @@ app.get("/get/rooms", (req, res) => {
   res.json(rooms_details);
 });
 
-io.on("connection", function(socket) {
-  socket.on("room_status_update", function(data) {
+io.on("connection", function (socket) {
+  socket.on("room_status_update", function (data) {
     socket.broadcast.emit("room_status_update", data);
   });
 });
 
-app.get("/manage_rooms", (req, res) => {
+app.get("/manage-rooms", (req, res) => {
   res.sendFile(__dirname + "/views/manage_room.html");
 });
 
-// app.listen(2000);
+app.use(function (req, res, next) {
+  res.status(404);
+  res.redirect("/");
+});
+
 server.listen(process.env.PORT || 8080);
